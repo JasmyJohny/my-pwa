5dc45d29a8d4f16839ee0a02f928db13aea7bfd7
Jasmy Thachangattu Johny <jasmylenson@Jasmys-MBP.ht.home>
2022-05-21 01:01:20 -0400

    commit changes

--- js/site.js
+++ js/site.js
@@ -1,23 +1,11 @@
-console.log(document);
-console.log(window);
-console.log(navigator.serviceWorker);
-if('serviceWorker' in navigator)
-{
-    //console.log("Yeah..");
-    navigator.serviceWorker.register('../service_worker.js');
-}
-else
-{
-    console.log("Noo");
-}
 
-// if ('serviceWorker' in navigator) {
+if ('serviceWorker' in navigator) {
 
-//     navigator.serviceWorker.register('../service-worker.js')
-//     .then((registration) => {
-//     console.log('Success. Scope:', registration.scope);
-//     })
-//     .catch((error) => {
-//     console.log('Failed. Error:', error);
-//     });
-// }
+    navigator.serviceWorker.register('../service-worker.js')
+    .then((registration) => {
+    console.log('Success. Scope:', registration.scope);
+    })
+    .catch((error) => {
+    console.log('Failed. Error:', error);
+    });
+}
